0260cb93d201daee0088bb9304cfbc812c0c6abf
claytoniousfunk <cbennett0715@gmail.com>
2023-07-05 10:02:18 -0500

    cleanup

--- triggerEmulation.sh
+++ triggerEmulation.sh
@@ -8,12 +8,13 @@ ERA="Run3_pp_on_PbPb"
 
 
 echo ""
-echo "ppRef trigger menu = $TRIGGERMENU"
+echo "[triggerEmulation] ppRef trigger menu = $TRIGGERMENU"
 echo ""
 
 LIMIT=1
 
-echo"Listing input filenames (test)"
+
+
 for((i=1; i <= LIMIT; i++)) ; do
 
     FILEPATH_i="root://cmsxrootd.fnal.gov//store/user/ddesouza/Dijet_TuneCP5_5p36TeV_ppref-pythia8/Dijet_TuneCP5_5p36TeV_ppref-pythia8/20230408PrivateDijet/230411_173439/0002/ppref_HINAutumn18GS-00032_py_GEN_SIM_$i.root"
@@ -25,16 +26,16 @@ for((i=1; i <= LIMIT; i++)) ; do
     echo "[triggerEmulation] editing ExportedMenu.py..."
 
     echo '
-# Define the output
-process.output = cms.OutputModule("PoolOutputModule",
-    outputCommands = cms.untracked.vstring("drop *", "keep *_TriggerResults_*_*", "keep *_hltTriggerSummaryAOD_*_*", "keep *_hltGtStage2Digis_*_*", "keep *_hltGtStage2ObjectMap_*_*"),
-    fileName = cms.untracked.string("output.root"),
-)
-process.output_path = cms.EndPath(process.output)
+      # Define the output
+      process.output = cms.OutputModule("PoolOutputModule",
+        outputCommands = cms.untracked.vstring("drop *", "keep *_TriggerResults_*_*", "keep *_hltTriggerSummaryAOD_*_*", "keep *_hltGtStage2Digis_*_*", "keep *_hltGtStage2ObjectMap_*_*"),
+        fileName = cms.untracked.string("output.root"),
+      )
+      process.output_path = cms.EndPath(process.output)
 
-process.schedule.append( process.output_path )  # enable this line if you want to get an output containing trigger info to be used for further analysis, e.g. "TriggerResults", digis etc
+      process.schedule.append( process.output_path )  # enable this line if you want to get an output containing trigger info to be used for further analysis, e.g. "TriggerResults", digis etc
 
-' >> ExportedMenu.py
+    ' >> ExportedMenu.py
 
 
 
